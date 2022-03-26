1dced84995ca0bb385e46766097db3d43c32db69
Lee Martin <hi@leemartin.com>
2022-03-26 14:38:38 -0500

    Add nuxt content and waveform images

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Taylor Hawkins</h1>
+  <section>
+    <h1>Taylor Hawkins</h1>
+
+    <div>{{ tracks }}</div>
+  </section>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    let tracks = await $content('tracks').fetch().catch(err => {
+      console.log(err)
+    })
+
+    return {
+      tracks
+    }
+  }
+}
+</script>
